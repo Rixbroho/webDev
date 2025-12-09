d4e21cd8c3787132d57934b76ce6d551f0e7f8c7
Rixbroho <ratnaprajapati10@gmail.com>
2025-12-09 13:36:11 +0545

    adding react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,12 @@ function App() {
   return (
     <>
     this is main routing page
-      <div className="flex">
+      <div className="flex w-100">
         <div className="text-3xl font-bold underline bg-red-500">
           Hello Alex
         </div>
         <div className="text-3xl font-bold underline bg-green-500">
-          Hello Suraj
+          Hello Nelish
         </div>
       </div>
     </>
