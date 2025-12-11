3f7d1897e97f8f25448ab4c7d8715325ce5d365a
Rixbroho <ratnaprajapati10@gmail.com>
2025-12-11 12:04:53 +0545

    adding folder and nodemon

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,19 @@ const app=express();
 const port=3000;
 
 app.get('/',(req,res)=>{
-    res.json({message:'Welcome to the Home Page'});
+    res.json({message:'Welcome to the Home Page from backend! change vayo wow'});
+});
+
+app.get('/auth/login',(req,res)=>{
+    res.json({message:'Welcome to login page'});
+});
+
+app.get('/auth/contact',(req,res)=>{
+    res.json({message:'Welcome to the contact page'});
+});
+
+app.get('/user',(req,res)=>{
+    res.json({message:'Welcome to user'});
 });
 
 app.listen(port,()=>{
