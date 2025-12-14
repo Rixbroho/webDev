7a248170be7a076940ed0674dc1f55e60e3ae3ee
Rixbroho <ratnaprajapati10@gmail.com>
2025-12-14 10:32:39 +0545

    adding usercontroller file

---
+++ backend/controllers/userController.js
@@ -0,0 +1,8 @@
+const getAllUser=async(req,res)=>{
+    res.json({message:"This is the get all user"});
+
+};
+
+module.exports={
+    getAllUser
+}
--- backend/index.js
+++ backend/index.js
@@ -2,21 +2,12 @@ const express=require('express');
 const app=express();
 const port=3000;
 
+app.use('/api/user/'.require('./routes/route'))
+
 app.get('/',(req,res)=>{
     res.json({message:'Welcome to the Home Page from backend! change vayo wow'});
 });
 
-app.get('/auth/login',(req,res)=>{
-    res.json({message:'Welcome to login page'});
-});
-
-app.get('/auth/contact',(req,res)=>{
-    res.json({message:'Welcome to the contact page'});
-});
-
-app.get('/user',(req,res)=>{
-    res.json({message:'Welcome to user'});
-});
 
 app.listen(port,()=>{
     console.log(`Server is running at http://localhost:${port}`);
