b5b9cc90a9257a47942b5c57816bfa4fed013cad
Rixbroho <ratnaprajapati10@gmail.com>
2025-12-21 11:14:55 +0545

    connnecting the database

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,3 +31,5 @@ const User=sequelize.define('User',{
     }
 
 );
+
+module.exports=User;
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -2,6 +2,8 @@ const express = require('express').Router();
 
 const{addUser}=require("../controllers/userController")
 
-express. post("/user",addUser)
+
+// express.get("/getalluser",getAllUser)
+express.post("/user",addUser)
 
 module.exports=express;
