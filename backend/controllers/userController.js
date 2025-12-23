c82b7c2dd23a45f2027f46c95e4741244006484f
Rixbroho <ratnaprajapati10@gmail.com>
2025-12-23 14:21:30 +0545

    updateuser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,8 +46,11 @@ const getAllUser=async(req,res)=>{
 
 const getUsersById=async(req,res)=>{
     try{
-        const {id}=req.params.id;
+        const id=req.params.id;
         const user=await User.findByPk(id);
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
         return res.json({
             user:{id:user.id,username:user.username},
             message:"User fetched successfully"
@@ -64,7 +67,31 @@ const getActiveUsers = async (req, res) => {
   res.json({ message: "Get active users - to be implemented" });
 };
 
+const updateUser=async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const {username,email,password}=req.body;
+        const user=await User.findByPk(id);
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+        let hassedPassword=user.password;
+        if(password){
+            hassedPassword=await bcrypt.hash(password,10);
+        }
+        await user.update({
+            username:username|| user.username,
+            email:email|| user.email,
+            password:hassedPassword,
+        });
+        return res.status(200).json({message:"User updated successfully",user});
+    }catch(error){
+        return res.status(500).json
+
+    }
+}
+
 
 module.exports={
-    getAllUser,addUser,getUsersById,getActiveUsers 
+    getAllUser,addUser,getUsersById,getActiveUsers,updateUser
 }
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,11 +1,12 @@
 const express = require('express').Router();
 
-const{addUser,getAllUser,getUsersById,getActiveUsers}=require("../controllers/userController")
+const{addUser,getAllUser,getUsersById,getActiveUsers,updateUser}=require("../controllers/userController")
 
 express.post("/user",addUser)
 express.get("/getalluser",getAllUser)
 express.get("/getusersbyid/:id",getUsersById)
 express.get("/getactiveusers",getActiveUsers)
+express.put("/updateuser",updateUser)
 
 
 module.exports=express;
