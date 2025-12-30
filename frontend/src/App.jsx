649d571f2cb7f03ba3effe495647a6676b06fa1e
Rixbroho <ratnaprajapati10@gmail.com>
2025-12-30 15:06:56 +0545

    adding coponent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,24 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Header from './pages/component/Header';
+import Footer from './pages/component/Footer';
 import { Toaster } from 'react-hot-toast';
 
 function App() {
   return(
     <Router>
       <Toaster/>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/register' element={<Register/>}></Route>
         <Route path='/about' element={<div>about</div>}></Route>
         <Route path='/contact' element={<div>contact</div>}></Route>
+        
       </Routes>
+      <Footer/>
     </Router>
   )
 }
---
+++ frontend/src/pages/component/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Header ho hai</div>
+  )
+}
+
+export default Header
