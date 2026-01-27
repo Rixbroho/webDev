7b43cf9ad4d62737ea146fca5831fcfd101e9230
Rixbroho <ratnaprajapati10@gmail.com>
2026-01-27 14:06:30 +0545

    testing

--- backend/test/api.test.js
+++ backend/test/api.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 require('dotenv').config();
 
-// const BASE_URL = `http://localhost:${process.env.PORT || 5000}`;
+const BASE_URL = `http://localhost:${process.env.PORT || 5000}`;
 
 // describe('open API tests', () => {
 //     it('should created a new user without an image',async () => {
